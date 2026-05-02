c51afb52646de8ec77623894791e73ba81f374d4
Haixing Hu <starfish.hu@gmail.com>
2026-05-02 16:23:48 +0800

    docs: add license headers to test modules

--- tests/classifier/mod.rs
+++ tests/classifier/mod.rs
@@ -1,2 +1,11 @@
+/*******************************************************************************
+ *
+ *    Copyright (c) 2025 - 2026 Haixing Hu.
+ *
+ *    SPDX-License-Identifier: Apache-2.0
+ *
+ *    Licensed under the Apache License, Version 2.0.
+ *
+ ******************************************************************************/
 mod ffprobe_command_media_stream_classifier_tests;
 mod media_stream_classifier_tests;
--- tests/detector/mod.rs
+++ tests/detector/mod.rs
@@ -1,3 +1,12 @@
+/*******************************************************************************
+ *
+ *    Copyright (c) 2025 - 2026 Haixing Hu.
+ *
+ *    SPDX-License-Identifier: Apache-2.0
+ *
+ *    Licensed under the Apache License, Version 2.0.
+ *
+ ******************************************************************************/
 mod abstract_mime_detector_tests;
 mod file_command_mime_detector_tests;
 mod mime_detector_tests;
